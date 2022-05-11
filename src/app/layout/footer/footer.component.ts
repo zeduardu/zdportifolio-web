98c7fe484e969fef58a978d936db668eb0020ae8
José Eduardo (Zeduardu) <zeduardu@gmail.com>
2022-05-10 21:58:28 -0300

    feat: added footer
    Added footer in every public layout

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDiscord, faGitSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  faLinkedin = faLinkedin;
+  faGitSquare = faGitSquare;
+  faDiscord = faDiscord;
 
   constructor() { }
 
